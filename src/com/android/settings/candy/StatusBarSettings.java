11cd2161857180a72eb0ed12e86e24a7c9e349d0
Arvind Mukund <armu30@gmail.com>
2015-03-03 13:32:11 +0530

    Hide unused msim icons on setting preference(1/2)
    Added a listPreference through which no-sim icons can be hidden or displayed
    as per the user's wish
    
    Change-Id: I9ffca4f4c394fb41d8d00ad550f06b4455b92350
    Signed-off-by: Arvind Mukund <armu30@gmail.com>
    
    Conflicts:
    	src/com/android/settings/candy/StatusBarSettings.java

--- src/com/android/settings/candy/StatusBarSettings.java
+++ src/com/android/settings/candy/StatusBarSettings.java
@@ -32,6 +32,7 @@ import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
+import android.telephony.TelephonyManager;
 import android.text.format.DateFormat;
 import android.view.View;
 
@@ -89,7 +90,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment
      
         mStatusBarClock = (ListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
         mStatusBarAmPm = (ListPreference) findPreference(STATUS_BAR_AM_PM);
-
         mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
         mStatusBarBatteryShowPercent =
                 (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
@@ -123,6 +123,10 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         mStatusBarBatteryShowPercent.setSummary(mStatusBarBatteryShowPercent.getEntry());
         enableStatusBarBatteryDependents(batteryStyle);
         mStatusBarBatteryShowPercent.setOnPreferenceChangeListener(this);
+
+        if (TelephonyManager.getDefault().getPhoneCount() <= 1) {
+            removePreference(Settings.System.STATUS_BAR_MSIM_SHOW_EMPTY_ICONS);
+        }
     }
 
     @Override
