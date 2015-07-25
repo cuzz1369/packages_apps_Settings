ec7eea8e1d870491991f257a2d806e50bc055aa1
rogersb11 <brettrogers11@gmail.com>
2015-07-24 20:55:22 -0400

    Themes tile 1/2
    Change-Id: I807fa97c9a8b462bce0be65dee397f01d337d7be
    
    Conflicts:
    	res/values/rr_strings.xml
    
    Conflicts:
    	res/values/rr_strings.xml
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -164,6 +164,10 @@ public class QSTileHolder {
 		resourceName = "ic_qs_candy_on";
 		stringId = R.string.qs_tile_candy;
 		break;
+            case QSConstants.TILE_THEMES:
+                resourceName = "ic_qs_themes";
+                stringId = R.string.qs_tile_themes;
+                break;
 
             // Dynamic tiles
             case QSConstants.DYNAMIC_TILE_NEXT_ALARM:
